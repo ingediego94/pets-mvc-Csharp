44e2943e6da406076243017e1884a903253dd925
ingediego94 <inge.diego94@gmail.com>
2025-10-01 22:55:49 -0500

    Second Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using practica_Mvc.Models;
+
+namespace practica_Mvc.Data;
+
+public class AppDbContext : DbContext
+{
+    // To create tables on DB:
+    public DbSet<Client> clients { get; set; }
+    public DbSet<Dog> dogs { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // To storage the connection string of our db
+        string db = "server=localhost;database=pets_db;user=root;password=Stauffemberg2%";
+        
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySql(
+                connectionString: db,
+                MySqlServerVersion.AutoDetect(db)
+            );
+        }
+    }
+}
